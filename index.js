4378bcb024a8e470dea67983ee00ead557e08dbf
Josefina Esnaola Borbore <josefinaesna@gmail.com>
2021-11-22 00:51:20 -0300

    Código listo - falta mostrar los datos

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const http = require('http');
+const express = require("express");
+const products = require('./product.js')
+const Contenedor = require('./contenedor')
+const fs = require("fs");
 const moment = require("moment");
 
-const server = http.createServer((request, response) => {
-  //cada vez que se hace un request se ejecuta
-  const now = new moment();
-  console.log(`Someone request page. At ${now.format("h:mm:ss a")}`);
-  response.end("<h1>Argentina clasifico a Qatar!!</h1>");
+const app = express();
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(products);
 });
 
-const connect = server.listen(3000, () => {
-  let port = connect.address().port;
-  console.log(`Escuchando por puerto ${port}`);
+app.on("error", (error) => {
+  console.log("Hubo un error");
+});
+
+app.get("/", (request, response) => {
+  response.send(`<h1 style='color:blue;'>Bienvenidos a Express</h1>`);
+});
+
+app.get("/productos", (request, response) => {
+  response.send(products.getAll())
+  /* fs.readFileSync("productos.txt", 'utf-8') */
+});
+
+app.get("/productoRandom", (request, response) => {
+  response.send(products.getById(Math.floor(Math.random() * (3 - 1 + 1) + 1)));
 });
--- product.js
+++ product.js
@@ -1,32 +1,23 @@
-const Contenedor = require('./desafio.js')
-
+const Contenedor = require('./contenedor.js')
+let products = new Contenedor('productos.txt')
 const run = async function () {
-    let contenedor = new Contenedor('productos.txt')
-    contenedor.save({
+    products.save({
         title: "Pulsera",
         price: 150,
         thumbnail: "shorturl.at/bqDZ3"
     })
 
-    contenedor.save({
+    products.save({
         title: "Collar",
         price: 100,
         thumbnail: "shorturl.at/hlEYZ"
     })
 
-    contenedor.save({
+    products.save({
         title: "iPhone",
         price: 1200,
         thumbnail: "shorturl.at/dpqH6"
     })
-    console.log(contenedor.getById(1));
-    console.log(contenedor.getById(5));
-    console.log(contenedor.getAll());
-    console.log(contenedor.deleteById(1));
-    console.log(contenedor.deleteById(6));
-    console.log(contenedor.getAll());
-    contenedor.deleteAll();
-    console.log(contenedor.getAll());
 };
 
-run();
+exports = products;
