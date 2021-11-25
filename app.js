f323a47a52df759cb4ff8535c9dc0b619d36532d
Josefina Esnaola Borbore <josefinaesna@gmail.com>
2021-11-25 00:33:32 -0300

    falta

--- app.js
+++ app.js
@@ -2,15 +2,37 @@ const express = require('express')
 
 const app = express()
 
-app.listen(process.env.PORT || 3000)
+app.listen(8080)
 
 app.get('/', (req, res) => {
     res.send(`
-        <a href="/saludo">Saludo<a>
+        <a href="/api/ciudades">Saludo<a>
     `)
 })
-app.get('/saludo', (req, res) => {
-    res.send(`
-        <h1>Hola Josefina!<h1>
-    `)
+
+let ciudades = [
+    {id: 1, nombre:'Bogotá', pais:'Colombia'},
+    {id: 2, nombre:'San Juan', pais:'Argentina'},
+    {id: 3, nombre:'Villa Mercedes', pais:'Argentina'},
+    {id: 1, nombre:'Medellin', pais:'Uruguay'}
+]
+
+app.get('/api/ciudades', (req, res) => {
+    console.log('GET recibido');
+    if (Object.entries(req.query).length > 0){
+        res.json({
+            result: ciudades.filter(c => c.pais===req.query.pais),
+            query: req.query
+        })
+    } else {
+        res.json(ciudades)
+    }
+})
+app.get('/api/ciudad/:id', (req, res) => {
+    console.log('GET recibido');
+    console.log(req.params.id);
+    let id = req.params.id
+    let ciudad = ciudades.find(c => c.id == id)
+    
+    res.json(ciudad)
 })
---
+++ frase.js
@@ -0,0 +1,40 @@
+const express = require('express')
+
+const app = express()
+
+app.listen(process.env.PORT || 3000)
+
+const frase = "Mientras más grande mejor"
+
+app.get('api/frase', (req, res) => {
+    res.json({frase})
+})
+
+app.get('api/letras/:num', (req, res) => { //Devuelve la letra en la posición indicada a través del param
+    let num = parseInt(req.params.num);
+
+    if(isNaN(num)) {
+        res.send({Error: "El parámetro no es número"})
+    }
+
+    if(num > frase.length || num<= 0){
+        res.send({Error: "Fuera de rango"})
+    }
+
+    res.json({letra: frase[num - 1]})
+})
+
+app.get('api/palabras/:num', (req, res) => { //Devuelve la palabra del número indicado. Ej: if 3 => grande
+    let palabras = frase.split(' ')
+    let num = parseInt(req.params.num);
+    
+    if(isNaN(num)) {
+        res.send({Error: "El parámetro no es número"})
+    }
+
+    if(num > palabras.length || num<= 0){
+        res.send({Error: "Fuera de rango"})
+    }
+
+    res.json({letra: palabras[num-1]})
+})
