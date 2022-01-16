9eb252ac1727d36f41e12f01e493db0759ce575b
Josefina Esnaola Borbore <josefinaesna@gmail.com>
2022-01-15 21:29:40 -0300

    Terminado

--- models/products.js
+++
@@ -1,8 +0,0 @@
-const UserSchema = new mongoose.Schema({
-    title: {type: String, require: true, max: 100},
-    price: Number,
-    thumbnail: {type: String, require: true}
-})
-
-const Products = mongoose.model('Products', UserSchema)
-module.exports = Products
